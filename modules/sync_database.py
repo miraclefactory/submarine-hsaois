ef8c41ab2ddc15fc410f2b610eb197a126496f8d
Zihan <sc20zz2@leeds.ac.uk>
2022-04-12 15:31:17 +0800

    sync_db

--- modules/sync_database.py
+++ modules/sync_database.py
@@ -1,3 +1,4 @@
+from os import sync
 import paramiko
 
 
@@ -6,27 +7,28 @@ def remove_db_x():
 
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-    client.connect(hostname='124.223.101.237', port=22, username='root', password='806710_zzh')
+    client.connect(hostname='124.223.101.237', port=22, username='root', password='zzh_xyl_123')
 
-    stdin, stdout, stderr = client.exec_command('cd /')
-    stdin, stdout, stderr = client.exec_command('cd www/wwwroot/hsaois_server/app')
-    stdin, stdout, stderr = client.exec_command('rm -rf hsaois.db')
+    stdin, stdout, stderr = client.exec_command('chown www /www/wwwroot/hsaois_server/app/hsaois.db')
 
-    localpath = './hsaois.db'
 
-    print(stdout.read().decode('utf-8'))
+
+
 
     client.close()
 
 
 def sync_db():
     tran = paramiko.Transport(('124.223.101.237', 22))
-    tran.connect(username="root", password='806710_zzh')
+    tran.connect(username="root", password='zzh_xyl_123')
     # 获取SFTP实例
     sftp = paramiko.SFTPClient.from_transport(tran)
     # 设置上传的本地/远程文件路径
-    localpath = "../hsaois.db"
+    localpath = "./hsaois.db"
     remotepath = "/www/wwwroot/hsaois_server/app/hsaois.db"
     sftp.remove(remotepath)
     sftp.put(localpath, remotepath)
     tran.close()
+
+sync_db()
+remove_db_x()
